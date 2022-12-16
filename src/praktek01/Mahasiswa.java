c49bea18bcf03c3e88fe49f25373ae6daa42be60
anamk-168 <anamk168@gmail.com>
2022-12-16 20:24:46 +0800

    Membuat Atribut dan Behavior

--- src/praktek01/Mahasiswa.java
+++ src/praktek01/Mahasiswa.java
@@ -11,4 +11,18 @@ package praktek01;
  */
 public class Mahasiswa {
     
+    String nama;
+    String NPM;
+    double IPK;
+    
+    void cetakInfo(){
+        
+        System.out.println("Nama  : "+nama);
+        System.out.println("NPM    : "+NPM);
+        System.out.println("IPK :"+IPK);
+    
+}
+
+
+
 }
